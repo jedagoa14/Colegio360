2e5ff5992641b53de03c72c62bf8ddb9e62e9eda
jedagoa14 <jedagoa14@gmail.com>
2026-08-04 15:23:09 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,71 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-whatsapp-image-2026-08-04-at-090357-1",
+      "name": "WhatsApp Image 2026-08-04 at 09.03.57 (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": -1.3772337309770109,
+        "pitch": -0.01703397621357361,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2637001329860276,
+          "pitch": 0.10595926555988555,
+          "rotation": 0,
+          "target": "1-whatsapp-image-2026-08-04-at-090356-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-whatsapp-image-2026-08-04-at-090356-1",
+      "name": "WhatsApp Image 2026-08-04 at 09.03.56 (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 2.1206851916958662,
+        "pitch": 0.16441442509128734,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1978970637393154,
+          "pitch": 0.11005230454178516,
+          "rotation": 0,
+          "target": "0-whatsapp-image-2026-08-04-at-090357-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Colegio",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
